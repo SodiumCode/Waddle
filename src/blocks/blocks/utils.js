cb9aad43d935e99bd0ead92a17cae940b07618e9
王晨帆 <wcf_studio@163.com>
2024-07-26 11:53:17 +0000

    update src/blocks/blocks/utils.js. 发现缺功能就不舒妥得慌
    Signed-off-by: 王晨帆 <wcf_studio@163.com>

--- src/blocks/blocks/utils.js
+++ src/blocks/blocks/utils.js
@@ -19,3 +19,14 @@ Blockly.Blocks["utils_isnative"] = {
     this.setHelpUrl("");
   },
 };
+
+Blockly.Blocks["utils_get_widget_image_url"] = {
+  init: function () {
+    this.appendValueInput("NAME").setCheck("String").appendField("获取文件名为");
+    this.appendDummyInput("").appendField("的素材库文件链接");
+    this.setOutput(true, "String");
+    this.setColour("#14b390");
+    this.setTooltip("例如'LOGO.jpg'");
+    this.setHelpUrl("");
+  },
+};
